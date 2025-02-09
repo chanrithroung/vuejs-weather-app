3fc2083da6d06f2c8ac02f34efa181ae0a0047ce
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2025-02-09 17:14:43 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,19 @@
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation></SiteNavigation>
 
-    <RouterView v-slot="{ Component }">
+    <!-- <RouterView v-slot="{ Component }">
       <Transition name="page" mode="out-in">
         <component :is="Component" /> 
       </Transition>
-    </RouterView>
+    </RouterView> -->
+    <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
-import HomeView from "./views/HomeView.vue";
 import SiteNavigation from "./components/SiteNavigation.vue";
+
 </script>
 
 <style scoped>
